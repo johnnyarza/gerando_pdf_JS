a1e9939c06cbda31ff3ed11391e2918573fc5e3b
johnnyarza <johnnyarza@gmail.com>
2021-08-08 20:26:56 -0300

    created table

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import PDFPrinter from 'pdfmake';
-import fs from 'fs';
 import Product from './database/models/Product';
 import mockData from './database/mockData';
 import fonts from './config/pdfFonts';
@@ -15,15 +14,69 @@ routes.get('/products', async (req, res) => {
 routes.get('/products/report', async (req, res) => {
   try {
     const printer = new PDFPrinter(fonts);
+    const products = await Product.findAll();
+
+    const body = products.map(({ id, description, price, quantity }, index) => {
+      const isOdd = index % 2 === 1;
+      return [id, description, price, quantity].map((text) => {
+        if (isOdd) return { text, style: 'oddRows' };
+        return { text };
+      });
+    });
+
     const docsDefinitions = {
-      defaultStyle: { font: 'Helvetica' },
-      content: [{ text: 'Meu Primeiro Relatório' }],
+      pageSize: 'A4',
+      pageOrientation: 'landscape',
+      defaultStyle: {
+        font: 'Helvetica',
+      },
+      content: [
+        {
+          columns: [
+            { text: 'Produtos', style: 'header' },
+            { text: `${new Date().toDateString()}\n\n`, style: 'header' },
+          ],
+        },
+        {
+          table: {
+            body: [
+              ['ID', 'Descrição', 'Preço', 'Quantidade'].map((text) => ({
+                text,
+                style: 'columnTitles',
+              })),
+              ...body,
+            ],
+          },
+        },
+        { qr: 'http://www.kkreco.duckdns.org:3000' },
+        { image: '' },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+        },
+        oddRows: { fillColor: '#ebebeb' },
+        columnTitles: {
+          fontSize: 12,
+          bold: true,
+          fillColor: '#2ecc71',
+          lineHeight: 1.5,
+        },
+      },
     };
     const pdfDoc = printer.createPdfKitDocument(docsDefinitions);
-    pdfDoc.pipe(fs.createWriteStream('Relatorio.pdf'));
+    // pdfDoc.pipe(fs.createWriteStream('Relatorio.pdf'));
 
+    const chuncks = [];
+    pdfDoc.on('data', (chunk) => chuncks.push(chunk));
     pdfDoc.end();
-    res.json('Realtorio concluído');
+
+    pdfDoc.on('end', () => {
+      const result = Buffer.concat(chuncks);
+      // res.setHeader('Content-Type', 'application/pdf');
+      res.end(result);
+    });
   } catch (error) {
     console.log(error);
   }
