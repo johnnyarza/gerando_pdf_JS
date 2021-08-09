a8c804a36c5ddc693677eb34e65c54a8c256172d
Johnny <johnnyarza@gmail.com>
2021-08-09 15:05:25 -0300

    header and footer

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import PDFPrinter from 'pdfmake';
+import QuickChart from 'quickchart-js';
+import { format } from 'date-fns';
+import fs from 'fs';
 import Product from './database/models/Product';
 import mockData from './database/mockData';
 import fonts from './config/pdfFonts';
 
 const routes = new Router();
+
+function imgToBuffer(file) {
+  const bitmap = fs.readFileSync(file);
+  return Buffer.from(bitmap);
+}
 
 routes.get('/', (req, res) => res.json({ message: 'Hello' }));
 routes.get('/products', async (req, res) => {
@@ -23,22 +31,82 @@ routes.get('/products/report', async (req, res) => {
         return { text };
       });
     });
+    const myChart = new QuickChart();
+    myChart
+      .setConfig({
+        type: 'bar',
+        data: {
+          labels: ['Hello world', 'Foo bar'],
+          datasets: [{ label: 'Foo', data: [1, 2] }],
+        },
+      })
+      .setWidth(400)
+      .setHeight(400)
+      .setBackgroundColor('transparent');
 
+    const chartBuffer = await myChart.toBinary();
     const docsDefinitions = {
       pageSize: 'A4',
       pageOrientation: 'landscape',
+      pageMargins: [40, 100, 40, 60],
       defaultStyle: {
         font: 'Helvetica',
       },
-      content: [
-        {
-          columns: [
-            { text: 'Produtos', style: 'header' },
-            { text: `${new Date().toDateString()}\n\n`, style: 'header' },
+      footer(currentPage, pageCount) {
+        return {
+          margin: [40, 20, 40, 20],
+          text: `${currentPage.toString()} / ${pageCount}`,
+          alignment: 'center',
+        };
+      },
+      header: {
+        // alignment: 'center',
+        // columns: [
+        //   { text: 'HJVA', style: 'header', alignment: 'center' },
+        //   {
+        //     image: imgToBuffer('src/assets/HJVA-logo.png'),
+        //     fit: [100, 100],
+        //   },
+        // ],
+        margin: [40, 20, 40, 20],
+        table: {
+          heights: [100],
+          widths: ['*', '*', '*'],
+
+          body: [
+            [
+              {
+                layout: 'noBorders',
+                table: {
+                  body: [
+                    [
+                      {
+                        image: imgToBuffer('src/assets/HJVA-logo.png'),
+                        fit: [120, 120],
+                      },
+                    ],
+                    [{ text: 'RELATORIO', style: 'header' }],
+                  ],
+                },
+              },
+              { text: 'RELATORIO', style: 'header', alignment: 'center' },
+              {
+                text: `HJVA \n ${format(
+                  new Date(),
+                  'dd-MM-yyyy HH:mm'
+                )} \n rua`,
+                style: 'header',
+                alignment: 'right',
+              },
+            ],
           ],
         },
+        layout: 'noBorders',
+      },
+      content: [
         {
           table: {
+            headerRows: 1,
             body: [
               ['ID', 'Descrição', 'Preço', 'Quantidade'].map((text) => ({
                 text,
@@ -49,11 +117,11 @@ routes.get('/products/report', async (req, res) => {
           },
         },
         { qr: 'http://www.kkreco.duckdns.org:3000' },
-        { image: '' },
+        { image: chartBuffer },
       ],
       styles: {
         header: {
-          fontSize: 18,
+          fontSize: 14,
           bold: true,
         },
         oddRows: { fillColor: '#ebebeb' },
